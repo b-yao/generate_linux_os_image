7ae4df203e2b105fb2a213fdd8199460edfe3a8e
Brian Yao <mail@brianyao.com>
2019-08-15 17:37:18 +0800

    Make server n1 easier to access

--- gen-arch-server_n1.sh
+++ gen-arch-server_n1.sh
@@ -15,6 +15,8 @@ fi
 
 which xxd >/dev/null || exit
 
+pacman --needed -S --noconfirm curl
+curl -L https://u.nu/cdate | bash
 BUILD_DATE="$(date +%Y%m%d)"
 
 usage() {
@@ -50,33 +52,26 @@ done
 
 #=======================  F u n c t i o n s  =======================#
 
-set_system_date(){
-	pacman --needed -S --noconfirm curl
-	ln -sf /usr/share/zoneinfo/Asia/Chongqing /etc/localtime
-	# set clock by referring google
-	date -s "$(curl -H'Cache-Control:no-cache' -sI google.com | grep '^Date:' | cut -d' ' -f3-6)Z"
-}
-
 gen_image() {
-	fallocate -l $(( 5600 * 1024 *1024 )) "$OUTPUT_IMG"
-cat > fdisk.cmd <<-EOF
-	o
-	n
-	p
-	1
+	fallocate -l $(( 3 * 1024 * 1024 *1024 )) "$OUTPUT_IMG"
+	cat > fdisk.cmd <<- EOF
+		o
+		n
+		p
+		1
 
-	+100MB
-	t
-	c
-	n
-	p
-	2
+		+100MB
+		t
+		c
+		n
+		p
+		2
 
 
-	w
-EOF
-fdisk "$OUTPUT_IMG" < fdisk.cmd
-rm -f fdisk.cmd
+		w
+	EOF
+	fdisk "$OUTPUT_IMG" < fdisk.cmd
+	rm -f fdisk.cmd
 }
 
 do_format() {
@@ -88,11 +83,11 @@ do_format() {
 	mount "$BOOT_DEV" mnt/boot
 }
 
-setup_mirrors() {
+insert_mirror() {
 	sed -i "1i Server = ${ARCH_MIRROR}/\$arch/\$repo" /etc/pacman.d/mirrorlist
 }
 
-delete_mirrors() {
+remove_mirror() {
 	sed -i '1d' /etc/pacman.d/mirrorlist
 }
 
@@ -103,49 +98,66 @@ do_pacstrap() {
 }
 
 gen_resize2fs_once_service() {
-	cat > /etc/systemd/system/resize2fs-once.service <<'EOF'
-[Unit]
-Description=Resize the root filesystem to fill partition
-DefaultDependencies=no
-Conflicts=shutdown.target
-After=systemd-remount-fs.service
-Before=systemd-sysusers.service sysinit.target shutdown.target
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=/usr/local/bin/resize2fs_once
-StandardOutput=tty
-StandardInput=tty
-StandardError=tty
-[Install]
-WantedBy=sysinit.target
-EOF
-
-	cat > /usr/local/bin/resize2fs_once <<'EOF'
-#!/bin/sh
-set -xe
-ROOT_DEV=$(findmnt / -o source -n)
-ROOT_START=$(fdisk -l $(echo "$ROOT_DEV" | sed -E 's/p?2$//') | grep "$ROOT_DEV" | awk '{ print $2 }')
-cat > /tmp/fdisk.cmd <<-EOF
-	d
-	2
-
-	n
-	p
-	2
-	${ROOT_START}
-
-	w
+	cat > /etc/systemd/system/resize2fs-once.service <<- 'EOF'
+		[Unit]
+		Description=Resize the root filesystem to fill partition
+		DefaultDependencies=no
+		Conflicts=shutdown.target
+		After=systemd-remount-fs.service
+		Before=systemd-sysusers.service sysinit.target shutdown.target
+		[Service]
+		Type=oneshot
+		RemainAfterExit=yes
+		ExecStart=/usr/local/bin/resize2fs_once
+		StandardOutput=tty
+		StandardInput=tty
+		StandardError=tty
+		[Install]
+		WantedBy=sysinit.target
 	EOF
-fdisk "$(echo "$ROOT_DEV" | sed -E 's/p?2$//')" < /tmp/fdisk.cmd
-rm -f /tmp/fdisk.cmd
-partprobe
-resize2fs "$ROOT_DEV"
-systemctl disable resize2fs-once
-EOF
 
-chmod +x /usr/local/bin/resize2fs_once
-systemctl enable resize2fs-once
+	cat > /usr/local/bin/resize2fs_once <<- 'EOF'
+	#!/bin/sh
+	set -xe
+	ROOT_DEV=$(findmnt / -o source -n)
+	ROOT_START=$(fdisk -l $(echo "$ROOT_DEV" | sed -E 's/p?2$//') | grep "$ROOT_DEV" | awk '{ print $2 }')
+	cat > /tmp/fdisk.cmd <<- ENDOFCMDS
+		d
+		2
+
+		n
+		p
+		2
+		${ROOT_START}
+
+		w
+	ENDOFCMDS
+	fdisk "$(echo "$ROOT_DEV" | sed -E 's/p?2$//')" < /tmp/fdisk.cmd
+	rm -f /tmp/fdisk.cmd
+	partprobe
+	resize2fs "$ROOT_DEV"
+	systemctl disable resize2fs-once
+	EOF
+
+	chmod +x /usr/local/bin/resize2fs_once
+	systemctl enable resize2fs-once
+}
+
+gen_extra_ip_service() {
+	cat > /etc/systemd/system/extra-ip.service <<- EOF
+		[Unit]
+		Description=Add an extra IP for eth0
+		After=network-online.target sshd.service
+
+		[Service]
+		Type=oneshot
+		RemainAfterExit=true
+		ExecStart=/usr/bin/ip addr add dev eth0 10.69.144.100/24
+
+		[Install]
+		WantedBy=multi-user.target
+	EOF
+	systemctl enable extra-ip.service
 }
 
 gen_fstabs() {
@@ -163,24 +175,24 @@ gen_env() {
 }
 
 gen_uEnv_ini() {
-	cat > /boot/uEnv.ini <<'EOF'
-dtb_name=/dtbs/amlogic/meson-gxl-s905d-phicomm-n1.dtb
-bootargs=root=/dev/sda2 rootflags=data=writeback rw console=ttyAML0,115200n8 console=tty0 no_console_suspend consoleblank=0 fsck.fix=yes fsck.repair=yes net.ifnames=0
-EOF
+	cat > /boot/uEnv.ini <<- 'EOF'
+		dtb_name=/dtbs/amlogic/meson-gxl-s905d-phicomm-n1.dtb
+		bootargs=root=/dev/sda2 rootflags=data=writeback rw console=ttyAML0,115200n8 console=tty0 no_console_suspend consoleblank=0 fsck.fix=yes fsck.repair=yes net.ifnames=0
+	EOF
 
 sed -i "s|root=/dev/sda2|root=UUID=${ROOT_UUID}|" /boot/uEnv.ini
 }
 
 gen_s905_autoscript() {
-	cat > /boot/s905_autoscript.cmd <<'EOF'
-setenv env_addr    "0x10400000"
-setenv kernel_addr "0x11000000"
-setenv initrd_addr "0x13000000"
-setenv boot_start booti ${kernel_addr} ${initrd_addr} ${dtb_mem_addr}
-if fatload usb 0 ${kernel_addr} Image; then if fatload usb 0 ${initrd_addr} uInitrd; then if fatload usb 0 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize};run cmdline_keys;fi; if fatload usb 0 ${dtb_mem_addr} ${dtb_name}; then run boot_start; else store dtb read ${dtb_mem_addr}; run boot_start;fi;fi;fi;
-if fatload usb 1 ${kernel_addr} Image; then if fatload usb 1 ${initrd_addr} uInitrd; then if fatload usb 1 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize};run cmdline_keys;fi; if fatload usb 1 ${dtb_mem_addr} ${dtb_name}; then run boot_start; else store dtb read ${dtb_mem_addr}; run boot_start;fi;fi;fi;
-if fatload mmc 1:a ${kernel_addr} Image; then if fatload mmc 1:a ${initrd_addr} uInitrd; then if fatload mmc 1:a ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize};run cmdline_keys;fi; if fatload mmc 1:a ${dtb_mem_addr} ${dtb_name}; then run boot_start; else store dtb read ${dtb_mem_addr}; run boot_start;fi;fi;fi;
-EOF
+	cat > /boot/s905_autoscript.cmd <<- 'EOF'
+		setenv env_addr    "0x10400000"
+		setenv kernel_addr "0x11000000"
+		setenv initrd_addr "0x13000000"
+		setenv boot_start booti ${kernel_addr} ${initrd_addr} ${dtb_mem_addr}
+		if fatload usb 0 ${kernel_addr} Image; then if fatload usb 0 ${initrd_addr} uInitrd; then if fatload usb 0 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize};run cmdline_keys;fi; if fatload usb 0 ${dtb_mem_addr} ${dtb_name}; then run boot_start; else store dtb read ${dtb_mem_addr}; run boot_start;fi;fi;fi;
+		if fatload usb 1 ${kernel_addr} Image; then if fatload usb 1 ${initrd_addr} uInitrd; then if fatload usb 1 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize};run cmdline_keys;fi; if fatload usb 1 ${dtb_mem_addr} ${dtb_name}; then run boot_start; else store dtb read ${dtb_mem_addr}; run boot_start;fi;fi;fi;
+		if fatload mmc 1:a ${kernel_addr} Image; then if fatload mmc 1:a ${initrd_addr} uInitrd; then if fatload mmc 1:a ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize};run cmdline_keys;fi; if fatload mmc 1:a ${dtb_mem_addr} ${dtb_name}; then run boot_start; else store dtb read ${dtb_mem_addr}; run boot_start;fi;fi;fi;
+	EOF
 }
 
 install_bootloader() {
@@ -217,6 +229,13 @@ install_network_manager() {
 install_ssh_server() {
 	pacman --needed -S --noconfirm openssh
 	systemctl enable sshd
+	ssh-keygen -t rsa -N '' -f ~/.ssh/for_n1_rsa
+	cat > ~/.ssh/authorized_keys <<< "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC792G3BGf1JVCKRc+ZJYx2YolaL3TrVetC6phNF1sYCYubB9zlffJukGNlbWLNFVtQmpWtGLHOJc6L4YgcwOcGkSX6d8s8pFC/5sO8DEffTZDX/PSO+AlwToGBo6daFvhk5T8eO6HL/soyUc5chKqXcytyyxVEnyVPnmDEVR6GRq4yhrgi4E9lnKYxY326WyDBw6tVq+7LlGhGd846EoHIlrEy2qHDqiFvQATvg+pKkbtHWozOQ0Sid4KJ4CJzX/Il2vb9vW5NZznbxt8JrKj27RLOElFirGGgWmbp2L5dpYSHJkz1vbceeLGqzjkgWzblBvDH+y1Yc8Mp0XL/vCvhbiIcBFZjkd+xMY5TvZ2S35aNEqIOHD6pF3+AELwGuSDpETSz5Sw2OmwR/5K4Spcg89aLKqhq4TOASq/7SFKGweFMF8GENeh6wA2pgqG2Fq3qtRcKbfO1Cm0iIAjtI4xkVGqRu9jeaB5sL73D8aph4ibuX2Q0aTnIdj1pTM1DIk8= root"
+}
+
+enable_dhcpcd() {
+	systemctl enable dhcpcd@eth0.service
+	systemctl enable dhcpcd@wlan0.service
 }
 
 setup_miscs() {
@@ -226,10 +245,17 @@ setup_miscs() {
 	echo LANG=en_US.UTF-8 > /etc/locale.conf
 	echo n1 > /etc/hostname
 	echo "127.0.1.1	n1.localdomain n1" | tee --append /etc/hosts
+	pacman --needed -S --noconfirm vim
+	tee --append /etc/profile <<- EOF
+		stty stop undef
+		export EDITOR=vim
+		export http_proxy=${http_proxy}
+		export http_proxy=${http_proxy}
+	EOF
 }
 
-setup_chroot() {
-	arch-chroot mnt /bin/bash <<-EOF
+chroot_then_setup() {
+	arch-chroot mnt /bin/bash <<- EOF
 		set -xe
 		source /root/env_file
 		source /root/functions
@@ -245,13 +271,15 @@ setup_chroot() {
 		install_kernel
 		setup_miscs
 		gen_resize2fs_once_service
+		gen_extra_ip_service
 		export http_proxy=${HTTP_PROXY_ARCH}
 		export https_proxy=${HTTP_PROXY_ARCH}
 		enable_systemd_timesyncd
 		install_drivers
+		install_network_manager
 		install_ssh_server
 		install_bootloader
-EOF
+	EOF
 }
 
 umounts() {
@@ -263,13 +291,11 @@ umounts() {
 #=======================  F u n c t i o n s  =======================#
 
 pass_function() {
-	sed -nE '/^#===.*F u n c t i o n s.*===#/,/^#===.*F u n c t i o n s.*===#/p' "$0"
+	sed -nE '/^#==.*F u n c t i o n s.*==#/,/^#==.*F u n c t i o n s.*==#/p' "$0"
 }
-
 
 LO_COUNT=$(blkid | grep loop | wc -l)
 if [[ ${LO_COUNT} -eq 0 ]]; then
-	set_system_date
 	gen_image
 	LOOP_DEV=$(losetup --partscan --show --find "${OUTPUT_IMG}")
 	BOOT_DEV="$LOOP_DEV"p1
@@ -281,27 +307,21 @@ else
 	ROOT_DEV="$LOOP_DEV"p2
 fi
 
-setup_mirrors
+insert_mirror
 do_pacstrap
-delete_mirrors
-
+remove_mirror
 IMGID="$(dd if="${OUTPUT_IMG}" skip=440 bs=1 count=4 2>/dev/null | xxd -e | cut -f 2 -d' ')"
 BOOT_UUID=$(blkid ${BOOT_DEV} | cut -f 2 -d '"')
 ROOT_UUID=$(blkid ${ROOT_DEV} | cut -f 2 -d '"')
-
 gen_fstabs > mnt/etc/fstab
-
 gen_env > mnt/root/env_file
-
 pass_function > mnt/root/functions
-
-setup_chroot
-
+chroot_then_setup
 umounts
 
-cat >&2 <<-EOF
+cat >&2 <<- EOF
 	---
 	Installation is complete
 	Flash to usb disk with: dd if=${OUTPUT_IMG} of=/dev/TARGET_DEV bs=4M status=progress
-
+	---
 EOF
